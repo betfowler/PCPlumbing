cdebe740d18e4c3e1ba83ca30e0b587520fe8326
Bethany <Bethany.fowler14@gmail.com>
2017-03-08 18:19:17 +0000

    Draft website complete - smtp needs to be changed later on

--- PCPlumbing/Controllers/HomeController.cs
+++ PCPlumbing/Controllers/HomeController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Net.Mail;
+using System.Threading.Tasks;
+using System.Net;
 
 namespace PCPlumbing.Controllers
 {
@@ -20,11 +23,60 @@ namespace PCPlumbing.Controllers
             return View();
         }
 
-        public ActionResult Contact(string name)
+        public async Task<ActionResult> Contact(string name, string email, string contact, string message)
         {
-            var test = name;
+            if (name != null)
+            {
+                //send email
+                var body = "<p>Name: {0}</p><p>Email address: {1}</p><p>Contact number: {2}</p><p>Message: {3}</p>";
+                string messageBody = string.Format(body, name, email, contact, message);
+                string to = "bethany.fowler14@gmail.com";
+                string from = "eStar.smtp.fowler@gmail.com";
+                string subject = "PC Plumbing Online Enquiry";
+
+                await SendMessage(to, from, messageBody, subject);
+
+                ViewBag.Success = "Your message has been sent to PC Plumbing";
+
+            }
             return View();
         }
+
+        public async Task<ActionResult> SendMessage(string to, string from, string body, string subject)
+        {
+            //send email
+            var message = new MailMessage();
+            message.Body = body;
+            message.To.Add(new MailAddress(to));
+            message.From = new MailAddress(from);
+            message.Subject = subject;
+            message.IsBodyHtml = true;
+
+            using (var smtp = new SmtpClient())
+            {
+                var credential = new NetworkCredential
+                {
+                    UserName = "estar.smtp.fowler@gmail.com",
+                    Password = "16eStar17"
+                };
+                smtp.Credentials = credential;
+                smtp.Host = "smtp.gmail.com";
+                smtp.Port = 587;
+                smtp.EnableSsl = true;
+
+                await smtp.SendMailAsync(message);
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult ContactForm()
+        {
+            
+            return View();
+        }
+        
 
         public ActionResult Plumbing()
         {
